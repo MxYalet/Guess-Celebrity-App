dc5d36af2b5bf89eee9fddcf155ff31c27497de9
yalet-wizard <YOU@EXAMPLE.COM>
2022-03-22 14:12:14 +0100

    first

---
+++ app/src/main/java/com/example/GuessCelebrityApp/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.GuessCelebrityApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebNames = new ArrayList<>();
+    ArrayList<String> celebURLs = new ArrayList<>();
+    String[]answers = new String[4];
+    ImageView imageView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    int chosenCeleb= 0;
+    int locationOfAnswer = 0;
+
+    public void celebChosen(View view) {
+        if (view.getTag().toString().equals(Integer.toString(locationOfAnswer))) {
+            Toast.makeText(getApplicationContext(), "CORRECT ANSWER", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "WRONG ANSWER, It was " + celebNames.get(chosenCeleb), Toast.LENGTH_SHORT).show();
+        }
+        newQuestion();
+    }
+
+    //Async task class for downloading the web images
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    //Async task class for downloading the web page
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public void newQuestion() {
+        try {
+            Random rand = new Random(celebURLs.size());
+            chosenCeleb = rand.nextInt(celebURLs.size());
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+            imageView.setImageBitmap(celebImage);
+
+            locationOfAnswer = rand.nextInt(4);
+            int incorrectAnswer;
+            for (int i = 0; i < 4; i++) {
+                if (i == locationOfAnswer) {
+                    answers[i] = celebNames.get(chosenCeleb);
+                } else {
+                    incorrectAnswer = rand.nextInt(celebURLs.size());
+                    while (incorrectAnswer == chosenCeleb) {
+                        incorrectAnswer = rand.nextInt(celebURLs.size());
+                    }
+                    answers[i] = celebNames.get(incorrectAnswer);
+                }
+            }
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+
+        try {
+            result = task.execute("https://www.imdb.com/list/ls052283250/").get();
+            String[] splitResult = result.split("<div class = \"lister-item-content\">");
+            Pattern p = Pattern.compile("src=\"(.*?)AL .jpg\"");
+            Matcher m = p.matcher(splitResult[0]);
+            while (m.find()) {
+                celebURLs.add(m.group(1));
+            }
+            p = Pattern.compile("img alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+            while (m.find()) {
+                celebNames.add(m.group(1));
+            }
+            newQuestion();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
